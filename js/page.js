8a7fcdb14f24f69b8bb2e7dd3e9fdbca5cf5adf9
DS19 <dimitarspasovski93@gmail.com>
2016-04-15 12:10:17 +0200

    responsive images

--- js/page.js
+++ js/page.js
@@ -9,10 +9,16 @@ $(function(){
     $("#navbarHolder").load("./navbar.html", function() {
         //navbar is loaded
 
+        var isTouchUsed = false;
+
+        //if touch is used instead of mouse, we don't want to activate the hover event
+        $("body").on("touchstart", function() {
+           isTouchUsed = true;
+        });
         var mq = window.matchMedia( "(min-width: 769px)" );
         $("ul.nav li.dropdown").hover(function() {
 
-                if (mq.matches) {
+                if (mq.matches && !isTouchUsed ) {
                     //simulate click
                     $(this).children("a").first().click();
                     $(this).find("b.fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
@@ -20,7 +26,7 @@ $(function(){
 
             },
             function() {
-                if(mq.matches) {
+                if(mq.matches && !isTouchUsed) {
                     $(this).removeClass("open");
                     $(this).find("b.fa-chevron-up").removeClass("fa-chevron-up").addClass("fa-chevron-down");
                 }
@@ -28,7 +34,8 @@ $(function(){
 
         $("ul.nav li.dropdown").click(function() {
             //change the arrow direction on click
-            if(!mq.matches) {
+            if(!mq.matches || isTouchUsed) {
+                console.log("I am here!");
                 if(lastSelectedItem == null) {
 
                     $(this).find("b.fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
