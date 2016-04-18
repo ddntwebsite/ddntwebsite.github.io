812f998ec5d9c79cab32e009042a48764d68a928
DS19 <dimitarspasovski93@gmail.com>
2016-04-18 13:08:59 +0200

    refactored jquery

--- js/page.js
+++ js/page.js
@@ -16,43 +16,34 @@ $(function(){
            isTouchUsed = true;
         });
         var mq = window.matchMedia( "(min-width: 769px)" );
+
         $("ul.nav li.dropdown").hover(function() {
+            if (mq.matches && !isTouchUsed ) {
+                //simulate click
+                $this = $(this);
+                $this.children("a").first().click();
+                $this.find("b.fa-chevron-down").attr("class", "fa fa-chevron-up");
+            }
+                },
 
-                if (mq.matches && !isTouchUsed ) {
-                    //simulate click
-                    $(this).children("a").first().click();
-                    $(this).find("b.fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-                }
-
-            },
             function() {
-                if(mq.matches && !isTouchUsed) {
-                    $(this).removeClass("open");
-                    $(this).find("b.fa-chevron-up").removeClass("fa-chevron-up").addClass("fa-chevron-down");
+                if (mq.matches && !isTouchUsed ) {
+                    $this = $(this);
+                    $this.removeClass("open");
+                    $this.find("b.fa-chevron-up").attr("class", "fa fa-chevron-down");
                 }
             });
+
+
 
         $("ul.nav li.dropdown").click(function() {
             //change the arrow direction on click
             if(!mq.matches || isTouchUsed) {
-                console.log("I am here!");
-                if(lastSelectedItem == null) {
 
-                    $(this).find("b.fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-                    lastSelectedItem = $(this).find("b.fa-chevron-up").first();
-                }
-
-                else if($(this).find("b.fa").first().hasClass("fa-chevron-up")) {
-
-                    lastSelectedItem = null;
-                    $(this).find("b.fa-chevron-up").removeClass("fa-chevron-up").addClass("fa-chevron-down");
-                }
-
-                else {
-                    lastSelectedItem.removeClass("fa-chevron-up").addClass("fa-chevron-down");
-                    $(this).find("b.fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-                    lastSelectedItem = $(this).find("b.fa-chevron-up").first();
-                }
+                var $this = $(this);
+                var $icon = $this.find('b');
+                $icon.attr('class', ($icon.hasClass('fa-chevron-up') ? 'fa fa-chevron-down' : 'fa fa-chevron-up'));
+                $this.siblings().find('b').attr('class', 'fa fa-chevron-down');
             }
         });
 
