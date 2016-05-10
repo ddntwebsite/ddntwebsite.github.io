6c1e5e24f1dbcb61ac02e08ed891184841f7591a
DS19 <dimitarspasovski93@gmail.com>
2016-05-10 21:24:48 +0200

    resized the spalsh images

--- js/page.js
+++ js/page.js
@@ -6,48 +6,7 @@ $(function(){
 
     var lastSelectedItem = null;
 
-    $("#navbarHolder").load("./navbar.html", function() {
-        //navbar is loaded
-
-        var isTouchUsed = false;
-
-        //if touch is used instead of mouse, we don't want to activate the hover event
-        $("body").on("touchstart", function() {
-           isTouchUsed = true;
-        });
-        var mq = window.matchMedia( "(min-width: 769px)" );
-
-        $("ul.nav li.dropdown").hover(function() {
-            if (mq.matches && !isTouchUsed ) {
-                //simulate click
-               var $this = $(this);
-                $this.children("a").first().click();
-                $this.find("b.fa-chevron-down").attr("class", "fa fa-chevron-up");
-            }
-                },
-
-            function() {
-                if (mq.matches && !isTouchUsed ) {
-                    var $this = $(this);
-                    $this.removeClass("open");
-                    $this.find("b.fa-chevron-up").attr("class", "fa fa-chevron-down");
-                }
-            });
-
-
-
-        $("ul.nav li.dropdown").click(function() {
-            //change the arrow direction on click
-            if(!mq.matches || isTouchUsed) {
-
-                var $this = $(this);
-                var $icon = $this.find('b');
-                $icon.attr('class', ($icon.hasClass('fa-chevron-up') ? 'fa fa-chevron-down' : 'fa fa-chevron-up'));
-                $this.siblings().find('b').attr('class', 'fa fa-chevron-down');
-            }
-        });
-
-    });
+    $("#navbarHolder").load("./navbar.html");
     $("#footerHolder").load("./footer.html");
 
 
